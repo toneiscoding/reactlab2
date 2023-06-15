6a1103dc7fbafae2716cf3a1a50824d0cbe97aa3
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-06-15 14:48:29 -0700

    adding server.js

---
+++ reactlab2/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Form = () => {
+    const []
+  return (
+    <div>
+        <input></input>
+        <input></input>
+        <input></input>
+        <button></button>
+    </div>
+  )
+}
+
+export default Form
